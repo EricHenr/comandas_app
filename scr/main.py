2ff869f7afe6af6474002cf8a6a27e5f28832b08
Abc Bolinhas <abc@bolinhas.com>
2024-05-15 15:30:31 -0300

    geração de telas

--- scr/main.py
+++ scr/main.py
@@ -5,6 +5,8 @@ from mod_funcionario.funcionario import bp_funcionario
 from mod_produto.produto import bp_produto
 from mod_cliente.cliente import bp_cliente
 from mod_index.index import bp_index
+from mod_login.login import bp_login
+from mod_erro.erro import bp_erro
 
 app = Flask(__name__)
 
@@ -12,9 +14,12 @@ app.register_blueprint(bp_funcionario)
 app.register_blueprint(bp_produto)
 app.register_blueprint(bp_cliente)
 app.register_blueprint(bp_index)
+app.register_blueprint(bp_login)
+app.register_blueprint(bp_erro)
+
 
 ''' chamadas dos formulários '''
-@app.route('/')
+@app.route('/home')
 def formIndex():
     return render_template('formIndex.html'), 200
 
